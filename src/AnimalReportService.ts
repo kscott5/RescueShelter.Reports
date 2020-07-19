a5bfac18bddcaf6b6613ff82332aaae9a27d8ef7
Karega K. Scott <kkscott@outlook.com>
2020-07-19 07:36:37 -0500

    Readonly data access

--- src/AnimalReportService.ts
+++ src/AnimalReportService.ts
@@ -1,3 +1,23 @@
+// NOTE: 
+//       Report Services contains readonly data access, and should be removed
+//       from Rescue Shelter Services project. Http Responses from these 
+//       Http Requests are a convention of ExpressJS Router and Ngnix proxy_pass. 
+//        
+// React UI example Http Requests without Ngnix where server
+//
+// http://[server]:3302/api/animal/new        [write]
+// http;//[server]:3302/api/animals           [readonly]
+// http://[server]:3303/api/report/categories [readonly]
+// 
+//
+// with Ngnix
+//
+// http://[server]/api/animal/new        [write]
+// http;//[server]/api/report/animals    [readonly]
+// http://[server]/api/report/categories [readonly]
+//
+// ADDITION EFFORT FOR PROOF OF CONCEPT [poc]
+//
 import {Application, Router} from "express";
 import bodyParser from "body-parser";
 
@@ -6,6 +26,58 @@ import {SecurityDb} from "../rescueshelter.services/src/securityservice"
 
 let model = services.getModel(services.ANIMAL_MODEL_NAME);
 let router = Router({ caseSensitive: true, mergeParams: true, strict: true});
+let _selectionFields = '_id name description imageSrc sponsors';
+
+async function getAnimal(id: String) : Promise<any>{
+    var data = await model.findById(id);
+    
+    return data;
+} 
+
+async function getAnimals(page: Number = 1, limit: Number = 5, phrase?: String) : Promise<any> {
+    var animalAggregate = (!phrase)? model.aggregate() :
+        model.aggregate().append({$match: {$text: {$search: phrase}}});
+            
+    var data = await animalAggregate.append([
+        {
+            $lookup: {
+                from: "sponsors",
+                let: {animals_sponsors: '$sponsors'},
+                pipeline: [{
+                    $project: {
+                        _id: false, useremail: 1, username: 1, 
+                        is_sponsor: {$in: ['$useremail', '$$animals_sponsors']}
+                    }            
+                }],
+                as: "sponsors"
+            }        
+        },
+        {
+        $project: {
+            name: 1, description: 1, endangered: 1, image: 1,
+            sponsors: {
+                $filter: {
+                    input: '$sponsors',
+                    as: 'contributor',
+                    cond: {$eq: ['$$contributor.is_sponsor', true]}
+                }
+            }
+        }}
+    ])
+    .limit(limit as number);
+    
+    return data;
+} 
+
+async function getCategories() : Promise<any> {
+    var data = await model.aggregate([
+        { '$sort':  { 'category': 1} },
+        { '$group': { '_id': '$category', 'count': { '$sum': 1}}},
+        { '$project': { 'category': 1, 'count': 1}}
+    ]);
+
+    return data;
+}
 
 export function PublishWebAPI(app: Application) : void {
         // Parser for various different custom JSON types as JSON
@@ -14,21 +86,50 @@ export function PublishWebAPI(app: Application) : void {
 
         let securityDb = new SecurityDb();
  
-        router.get('/categories', jsonBodyParser, async (req,res) => {
+        router.get('/animal/categories', jsonBodyParser, async (req,res) => {
             res.status(200);
 
             try {
-                var data = await model.aggregate([
-                    { '$sort':  { 'category': 1} },
-                    { '$group': { '_id': '$category', 'count': { '$sum': 1}}},
-                    { '$project': { 'category': 1, 'count': 1}}
-                ]);
+                var data = await getCategories();
+
+                res.json(jsonResponse.createData(data));
             } catch(error) {
                 res.json(jsonResponse.createError(error));
             }
-
-            res.json(jsonResponse.createData(data));
         });
+
+        router.get("/animals", async (req,res) => {
+            console.debug(`GET: ${req.url}`);
+            var page = Number.parseInt(req.query["page"] as any || 1); 
+            var limit = Number.parseInt(req.query["limit"] as any || 5);
+            var phrase = req.query["phrase"] as string || '';
+
+            res.status(200);
+            
+            try {
+                var data = await getAnimals(page, limit, phrase);
+                res.json(jsonResponse.createPagination(data,1,page));
+            } catch(error) {
+                res.json(jsonResponse.createError(error));
+            }
+        }); //end routeAnimals
+
+        router.get('/animal/:id', jsonBodyParser, async (req,res) => {
+            console.debug(`GET: ${req.url}`);
+            if (!req.params.id) {
+                res.status(404);
+                res.send("HttpGET id not available");
+                return;
+            }
+            res.status(200);
+            try {
+                var data = await model.findById(req.params.id);
+                res.json(jsonResponse.createData(data));
+            } catch(error) {
+                    console.log(error);
+                    res.json(jsonResponse.createError(error));
+            }
+        }); 
 
         app.use('/api/report', router);
 } // end PublishWebAPI
