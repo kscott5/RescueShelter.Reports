a9e443e5183052638186fce96901606a6566cd95
Karega K. Scott <kkscott@outlook.com>
2020-07-23 17:34:29 -0500

    update version and port #

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ import {CoreServer} from "rescueshelter.core";
 import {PublishWebAPI as AnimalReportServices} from "./AnimalReportService";
 import {PublishWebAPI as SponsorReportServices} from "./SponsorReportService";
 
-CoreServer.start('Rescue Shelter Report Services', 3302, 
+CoreServer.start('Rescue Shelter Report Services', 3303, 
     [AnimalReportServices,SponsorReportServices], [], './public');
