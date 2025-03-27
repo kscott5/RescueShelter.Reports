e2d75c92d84cb3fd4e9e05487b19574407e09166
Karega K. Scott <kkscott@outlook.com>
2025-03-27 15:21:21 -0500

    set wwwroot static path and heading change

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,10 @@ import {CoreServer} from "rescueshelter.core";
 import {PublishWebAPI as AnimalReportServices} from "./AnimalReportService";
 import {PublishWebAPI as SponsorReportServices} from "./SponsorReportService";
 
+declare let __dirname; // variable initialize by NodeJS Path Module
+
+let path = require("path");
+let staticPath = path.join(__dirname, '/../public');
+
 CoreServer.start('Rescue Shelter Report Services', 3303, 
-    [AnimalReportServices,SponsorReportServices], [], './public');
+    [AnimalReportServices,SponsorReportServices], [], staticPath);
